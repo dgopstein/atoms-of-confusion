8b32c23021d1d241db1724375b13a36af773fcb9
Dan Gopstein <dan@gopstein.com>
2015-09-26 10:11:03 -0400

    add conditionals file

---
+++ conditionals.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdio.h>
+
+// Compare a number against a different number
+void conditional_assignment() {
+  int v1 = 7;
+  if ((v1 = 8)) printf("true\n"); else printf("false\n");
+}
+//int main() { int v1 = 7; if (v1 = 8) printf("true\n"); else printf("false\n"); }
+
+// Compare two identical strings
+void compare_strings() {
+  if (strcmp("a", "a")) printf("true\n"); else printf("false\n");
+}
+int main() { if (strcmp("a", "a")) printf("true\n"); else printf("false\n"); }
+
+//int main() {
+//  //conditional_assignment();
+//  //compare_strings();
+//}
