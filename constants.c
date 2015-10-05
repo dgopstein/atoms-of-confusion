246a2a332cf10d327ed13171015dd6bdcc294ec0
Dan Gopstein <dan@gopstein.com>
2015-10-05 10:33:52 -0400

    Add constants.c

---
+++ constants.c
@@ -0,0 +1,13 @@
+// Calculate distance travelled of a falling body
+
+double f1(double p1, double p2, double p3) {
+  return p1 * p2 * p3 * p3;
+}
+
+int main() {
+  printf("%f\n", f1(0.5, 9.8, 3)); 
+}
+
+
+
+
