e5c3f300b8746f14dc3400dd7f57456797c1161b
Dan Gopstein <dan@gopstein.com>
2015-12-11 16:44:35 -0500

    Unnormalizable code

--- simplifications/endoh2/scratch.c
+++ simplifications/endoh2/scratch.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
-#define COMMAC    q=/*XYXY*/
 
 int main() {
+  // This is code where comments give necessary whitespace
   int/**/x = 1;
   int    y = 2;
   printf("x y: %d %d\n", x, y);
 
+  // This shows that macros don't make substitutions inside of strings
+  #define COMMAC q=/*XYXY*/
   int COMMAC 2;
   printf(" COMMAC : %d\n", q);
+
+  // Unnormalizable code
+  #define f(a) #a
+  int hello, world;
+  printf("%s %s!\n", f(hello), f(world));
 
   return 0;
 }
