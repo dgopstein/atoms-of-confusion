460ca69ca414e850bd1d248853269bd7e0aabebf
Dan Gopstein <dan@gopstein.com>
2016-01-11 14:56:44 -0500

    Add program study question code

---
+++ program_study/question1.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+#define M1 char
+#define M2 putchar
+#define M3 while
+#define M4 int
+#define M5 main
+#define M6 if
+#define M7 '\n'
+M1 *V1 [] = {
+"]kh",
+"]IZh",
+'\0'};
+M1 *V2; M4 V3,V4,V5=0x59,V6=0x29,
+V7=0x68;M5(){V4=0;M3(V1[V4]){V2=V1[V4++];
+M3(*V2){V3= *V2++-V5;M3(V3--)M2(*V2-V6);
+M6(*V2==V7)M2(M7);*V2++;}}}
---
+++ program_study/question2.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#define TA v4 =
+#define CG v5 =
+
+#define Y(a3)                                                                  \
+  v1;                                                                          \
+  a2++; \
+  if (a2 % 2) { \
+    v2 = v2 * 4 + a3; \
+    if (v2 && a2 % 8 > 5) { \
+      f1(1, v2); \
+      v2 = 0;\
+    } \
+  } \
+  v1 =
+
+#define C Y(1)
+#define G Y(2)
+#define A Y(0)
+
+#define AT                                                                     \
+  int m(void) {                                                                \
+  v1 =
+
+#define T Y(3)
+#define GC                                                                     \
+  v1;                                                                          \
+  return 0;                                                                    \
+  }                                                                            \
+  int (*v3)(void) = m;
+
+void f1(int a1, int a2) {
+  while (a1--) {
+    putchar(a2);
+  }
+}
+int (*v3)(void), v1, a2, v2, v4, v5;
+void f2() {
+  if (!v1) {
+    v2 = getchar();
+    if (v2 < 0) {
+      v2 = 0;
+    }
+    v1 = 64;
+  }
+  a2++;
+  v4 = a2 % 8;
+  v4 =(1 + v4 * (7 - v4)) / 3;
+  v5 = a2 * (15 - a2) - 36;
+  int v6 = !v4 + 4;
+  f1(v6 + (v5 < 0 ? 0 : v5 / 6), ' ');
+}
+int main() {
+  if (v3) {
+    v3();
+  } else {
+    puts("#include \"prog.c\"\n\n     AT");
+    for (; f2(), v4 || v2; a2 %= 16) {
+      if (v4) {
+        int v7 = (v2 / v1) & 3;
+        f1(1, "ACGT"[v7]);
+        f1(v4, '~');
+        f1(1, "TGCA"[v7]);
+        v1 /= 4;
+        f1(1, '\n');
+      } else {
+        puts(a2 % 8 ? "CG" : "TA");
+      }
+    }
+    puts("GC");
+  }
+  return 0;
+}
