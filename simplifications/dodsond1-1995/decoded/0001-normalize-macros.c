48ef53188ed9d9ea88bf39dc961e0f92ad2da057
Dan Gopstein <dan@gopstein.com>
2016-01-18 17:51:13 -0500

    normalize dodson

---
+++ simplifications/dodsond1-1995/decoded/0001-normalize-macros.c
@@ -0,0 +1,67 @@
+#define M01
+#define M02
+#define M03
+#define M04
+#define M05
+#define M06
+#define M07
+#define M08 for
+#define M09
+#define M10 char
+#define M11 main
+#define M12 getchar
+#define M13 putchar
+
+     M01                                       M01
+    M01 M01                                     M01 M01
+   M01   M01                                   M01   M01
+   M01    M01                                 M01    M01
+  M01      M01                               M01      M01
+  M01       M01                             M01       M01
+ M01         M01                           M01         M01
+ M01   M01     M01                           M01     M01   M01
+ M01   M02     M01                         M01     M02   M01
+M01    M03    M01        M09        M01    M03    M01
+M01     M03    M01   M04         M04   M01    M03     M01
+M01     M04   M01 M02 M11(){ M10  M02 M01   M04     M01
+M01     M04    M01   oink[9],*igpa,    M01    M04     M01
+M01     M06 atinla=M12(),iocccwa M06     M01
+M01      M04 ,apca='A',owla='a',umna=26  M04      M01
+M01      M03  ; M08(; (atinla+1)&&(!(((   M03      M01
+M01      M02 atinla-apca)*(apca+umna-atinla) M02      M01
+ M01     M01  >=0)+((atinla-owla)*(owla+umna-  M01     M01
+ M01       atinla)>=0))); M13(atinla),        M01
+ M01   M01 atinla=M12()); M08(; atinla+1;  M01   M01
+  M01 M01  ){ M08(      igpa=oink     ,iocccwa=( M01 M01
+  M01 M01  (atinla-  M03  apca)*(  M03 apca+umna- M01 M01
+   M01 atinla)>=0) M03           M03   ; ((((    M01
+  M01 atinla-apca M05 M07 M05  )*(apca+   M01
+  M01 umna-atinla M06 )>=0) M06 +((atinla-  M01
+ M01 owla)*(owla+ M04   umna-   M04 atinla)>=0)) M01
+ M01   &&"-Pig-"   M02  "Lat-in"   M02   "COb-fus"   M01
+ M01 "ca-tion!!"[  M01  (((atinla-   M01  apca)*(apca+ M01
+ M01 umna-atinla) M01  >=0)?atinla-   M01  apca+owla:  M01
+M01 atinla)-owla M01 ]-'-')||((igpa==  M01 oink)&&!(*(  M01
+M01 igpa++)='w') M01 )||! M01 (*( M01 igpa M01 ++)=owla); * M01
+M01 (igpa++)=(( M01  (   M03   M03      M01 atinla-apca M01
+M01  )*(apca+   M01 umna M03 - M03      M01 atinla)>=0) M01
+M01  ?atinla-   M01 apca M03 + M03 owla M01  :atinla),  M01
+ M01   atinla=   M01      M01     M01      M01 M12()) M01
+ M01   ; M08(   M01 atinla=iocccwa?(( M01  (atinla-   M01
+ M01 owla)*(owla+ M01 umna-atinla)>=0 M01  )?atinla-   M01
+ M01  owla+apca:   M01   atinla):    M01  atinla; (((  M01
+  M01 atinla-apca)* M01 (apca+umna- M01 atinla)>=0)+( M01
+  M01 (atinla-owla)* M01  (owla+   M01 umna-atinla)>= M01
+   M01 0)); M13( M02       M02 atinla),atinla M01
+   M01  =M12());   M07  M08(*igpa=0,   M01
+    M01  igpa=oink; *           igpa; M13( M01
+     M01 *(igpa++))); M08(; (atinla+1)&&(!((( M01
+      M01 atinla-apca              )*(apca+   M01
+       M01   umna-    M05 M05 atinla)>=0 M01
+        M01   )+((       M05     atinla-  M01
+         M02  owla)*(         owla+umna- M02
+           M02 atinla)>=0))); M13 M02
+             M02  (atinla),atinla=   M02
+               M02 M12()); }  M02
+                 M04   }     M04
+                     M09
---
+++ simplifications/dodsond1-1995/decoded/0002-normalize-variables.c
@@ -0,0 +1,67 @@
+#define M01
+#define M02
+#define M03
+#define M04
+#define M05
+#define M06
+#define M07
+#define M08 for
+#define M09
+#define M10 char
+#define M11 main
+#define M12 getchar
+#define M13 putchar
+
+     M01                                       M01
+    M01 M01                                     M01 M01
+   M01   M01                                   M01   M01
+   M01    M01                                 M01    M01
+  M01      M01                               M01      M01
+  M01       M01                             M01       M01
+ M01         M01                           M01         M01
+ M01   M01     M01                           M01     M01   M01
+ M01   M02     M01                         M01     M02   M01
+M01    M03    M01        M09        M01    M03    M01
+M01     M03    M01   M04         M04   M01    M03     M01
+M01     M04   M01 M02 M11(){ M10  M02 M01   M04     M01
+M01     M04    M01   V1[9],*V2,    M01    M04     M01
+M01     M06 V3=M12(),V4 M06     M01
+M01      M04 ,V5='A',V6='a',V7=26  M04      M01
+M01      M03  ; M08(; (V3+1)&&(!(((   M03      M01
+M01      M02 V3-V5)*(V5+V7-V3) M02      M01
+ M01     M01  >=0)+((V3-V6)*(V6+V7-  M01     M01
+ M01       V3)>=0))); M13(V3),        M01
+ M01   M01 V3=M12()); M08(; V3+1;  M01   M01
+  M01 M01  ){ M08(      V2=V1     ,V4=( M01 M01
+  M01 M01  (V3-  M03  V5)*(  M03 V5+V7- M01 M01
+   M01 V3)>=0) M03           M03   ; ((((    M01
+  M01 V3-V5 M05 M07 M05  )*(V5+   M01
+  M01 V7-V3 M06 )>=0) M06 +((V3-  M01
+ M01 V6)*(V6+ M04   V7-   M04 V3)>=0)) M01
+ M01   &&"-Pig-"   M02  "Lat-in"   M02   "COb-fus"   M01
+ M01 "ca-tion!!"[  M01  (((V3-   M01  V5)*(V5+ M01
+ M01 V7-V3) M01  >=0)?V3-   M01  V5+V6:  M01
+M01 V3)-V6 M01 ]-'-')||((V2==  M01 V1)&&!(*(  M01
+M01 V2++)='w') M01 )||! M01 (*( M01 V2 M01 ++)=V6); * M01
+M01 (V2++)=(( M01  (   M03   M03      M01 V3-V5 M01
+M01  )*(V5+   M01 V7 M03 - M03      M01 V3)>=0) M01
+M01  ?V3-   M01 V5 M03 + M03 V6 M01  :V3),  M01
+ M01   V3=   M01      M01     M01      M01 M12()) M01
+ M01   ; M08(   M01 V3=V4?(( M01  (V3-   M01
+ M01 V6)*(V6+ M01 V7-V3)>=0 M01  )?V3-   M01
+ M01  V6+V5:   M01   V3):    M01  V3; (((  M01
+  M01 V3-V5)* M01 (V5+V7- M01 V3)>=0)+( M01
+  M01 (V3-V6)* M01  (V6+   M01 V7-V3)>= M01
+   M01 0)); M13( M02       M02 V3),V3 M01
+   M01  =M12());   M07  M08(*V2=0,   M01
+    M01  V2=V1; *           V2; M13( M01
+     M01 *(V2++))); M08(; (V3+1)&&(!((( M01
+      M01 V3-V5              )*(V5+   M01
+       M01   V7-    M05 M05 V3)>=0 M01
+        M01   )+((       M05     V3-  M01
+         M02  V6)*(         V6+V7- M02
+           M02 V3)>=0))); M13 M02
+             M02  (V3),V3=   M02
+               M02 M12()); }  M02
+                 M04   }     M04
+                     M09
