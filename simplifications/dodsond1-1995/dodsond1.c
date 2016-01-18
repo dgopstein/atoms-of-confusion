58dcb67b686e1a35c39dc04c203eb93e2cd6e915
Dan Gopstein <dan@gopstein.com>
2016-01-18 17:51:01 -0500

    normalize dodson

--- simplifications/dodsond1-1995/dodsond1.c
+++ simplifications/dodsond1-1995/dodsond1.c
@@ -1,67 +1,67 @@
-#define X
-#define XX
-#define XXX
-#define XXXX
-#define XXXXX
-#define XXXXXX
-#define XXXXXXX
-#define orfa for
-#define XXXXXXXXX
-#define archa char
-#define ainma main
-#define etcharga getchar
-#define utcharpa putchar
+#define M01
+#define M02
+#define M03
+#define M04
+#define M05
+#define M06
+#define M07
+#define M08 for
+#define M09
+#define M10 char
+#define M11 main
+#define M12 getchar
+#define M13 putchar
 
-     X                                       X
-    X X                                     X X
-   X   X                                   X   X
-   X    X                                 X    X
-  X      X                               X      X
-  X       X                             X       X
- X         X                           X         X
- X   X     X                           X     X   X
- X   XX     X                         X     XX   X
-X    XXX    X        XXXXXXXXX        X    XXX    X
-X     XXX    X   XXXX         XXXX   X    XXX     X
-X     XXXX   X XX ainma(){ archa  XX X   XXXX     X
-X     XXXX    X   oink[9],*igpa,    X    XXXX     X
-X     XXXXXX atinla=etcharga(),iocccwa XXXXXX     X
-X      XXXX ,apca='A',owla='a',umna=26  XXXX      X
-X      XXX  ; orfa(; (atinla+1)&&(!(((   XXX      X
-X      XX atinla-apca)*(apca+umna-atinla) XX      X
- X     X  >=0)+((atinla-owla)*(owla+umna-  X     X
- X       atinla)>=0))); utcharpa(atinla),        X
- X   X atinla=etcharga()); orfa(; atinla+1;  X   X
-  X X  ){ orfa(      igpa=oink     ,iocccwa=( X X
-  X X  (atinla-  XXX  apca)*(  XXX apca+umna- X X
-   X atinla)>=0) XXX           XXX   ; ((((    X
-  X atinla-apca XXXXX XXXXXXX XXXXX  )*(apca+   X
-  X umna-atinla XXXXXX )>=0) XXXXXX +((atinla-  X
- X owla)*(owla+ XXXX   umna-   XXXX atinla)>=0)) X
- X   &&"-Pig-"   XX  "Lat-in"   XX   "COb-fus"   X
- X "ca-tion!!"[  X  (((atinla-   X  apca)*(apca+ X
- X umna-atinla) X  >=0)?atinla-   X  apca+owla:  X
-X atinla)-owla X ]-'-')||((igpa==  X oink)&&!(*(  X
-X igpa++)='w') X )||! X (*( X igpa X ++)=owla); * X
-X (igpa++)=(( X  (   XXX   XXX      X atinla-apca X
-X  )*(apca+   X umna XXX - XXX      X atinla)>=0) X
-X  ?atinla-   X apca XXX + XXX owla X  :atinla),  X
- X   atinla=   X      X     X      X etcharga()) X
- X   ; orfa(   X atinla=iocccwa?(( X  (atinla-   X
- X owla)*(owla+ X umna-atinla)>=0 X  )?atinla-   X
- X  owla+apca:   X   atinla):    X  atinla; (((  X
-  X atinla-apca)* X (apca+umna- X atinla)>=0)+( X
-  X (atinla-owla)* X  (owla+   X umna-atinla)>= X
-   X 0)); utcharpa( XX       XX atinla),atinla X
-   X  =etcharga());   XXXXXXX  orfa(*igpa=0,   X
-    X  igpa=oink; *           igpa; utcharpa( X
-     X *(igpa++))); orfa(; (atinla+1)&&(!((( X
-      X atinla-apca              )*(apca+   X
-       X   umna-    XXXXX XXXXX atinla)>=0 X
-        X   )+((       XXXXX     atinla-  X
-         XX  owla)*(         owla+umna- XX
-           XX atinla)>=0))); utcharpa XX
-             XX  (atinla),atinla=   XX
-               XX etcharga()); }  XX
-                 XXXX   }     XXXX
-                     XXXXXXXXX
+     M01                                       M01
+    M01 M01                                     M01 M01
+   M01   M01                                   M01   M01
+   M01    M01                                 M01    M01
+  M01      M01                               M01      M01
+  M01       M01                             M01       M01
+ M01         M01                           M01         M01
+ M01   M01     M01                           M01     M01   M01
+ M01   M02     M01                         M01     M02   M01
+M01    M03    M01        M09        M01    M03    M01
+M01     M03    M01   M04         M04   M01    M03     M01
+M01     M04   M01 M02 M11(){ M10  M02 M01   M04     M01
+M01     M04    M01   V1[9],*V2,    M01    M04     M01
+M01     M06 V3=M12(),V4 M06     M01
+M01      M04 ,V5='A',V6='a',V7=26  M04      M01
+M01      M03  ; M08(; (V3+1)&&(!(((   M03      M01
+M01      M02 V3-V5)*(V5+V7-V3) M02      M01
+ M01     M01  >=0)+((V3-V6)*(V6+V7-  M01     M01
+ M01       V3)>=0))); M13(V3),        M01
+ M01   M01 V3=M12()); M08(; V3+1;  M01   M01
+  M01 M01  ){ M08(      V2=V1     ,V4=( M01 M01
+  M01 M01  (V3-  M03  V5)*(  M03 V5+V7- M01 M01
+   M01 V3)>=0) M03           M03   ; ((((    M01
+  M01 V3-V5 M05 M07 M05  )*(V5+   M01
+  M01 V7-V3 M06 )>=0) M06 +((V3-  M01
+ M01 V6)*(V6+ M04   V7-   M04 V3)>=0)) M01
+ M01   &&"-Pig-"   M02  "Lat-in"   M02   "COb-fus"   M01
+ M01 "ca-tion!!"[  M01  (((V3-   M01  V5)*(V5+ M01
+ M01 V7-V3) M01  >=0)?V3-   M01  V5+V6:  M01
+M01 V3)-V6 M01 ]-'-')||((V2==  M01 V1)&&!(*(  M01
+M01 V2++)='w') M01 )||! M01 (*( M01 V2 M01 ++)=V6); * M01
+M01 (V2++)=(( M01  (   M03   M03      M01 V3-V5 M01
+M01  )*(V5+   M01 V7 M03 - M03      M01 V3)>=0) M01
+M01  ?V3-   M01 V5 M03 + M03 V6 M01  :V3),  M01
+ M01   V3=   M01      M01     M01      M01 M12()) M01
+ M01   ; M08(   M01 V3=V4?(( M01  (V3-   M01
+ M01 V6)*(V6+ M01 V7-V3)>=0 M01  )?V3-   M01
+ M01  V6+V5:   M01   V3):    M01  V3; (((  M01
+  M01 V3-V5)* M01 (V5+V7- M01 V3)>=0)+( M01
+  M01 (V3-V6)* M01  (V6+   M01 V7-V3)>= M01
+   M01 0)); M13( M02       M02 V3),V3 M01
+   M01  =M12());   M07  M08(*V2=0,   M01
+    M01  V2=V1; *           V2; M13( M01
+     M01 *(V2++))); M08(; (V3+1)&&(!((( M01
+      M01 V3-V5              )*(V5+   M01
+       M01   V7-    M05 M05 V3)>=0 M01
+        M01   )+((       M05     V3-  M01
+         M02  V6)*(         V6+V7- M02
+           M02 V3)>=0))); M13 M02
+             M02  (V3),V3=   M02
+               M02 M12()); }  M02
+                 M04   }     M04
+                     M09
