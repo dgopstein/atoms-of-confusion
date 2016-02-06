73261bfe4ed12237015b8d01fb3df75e4ba0537d
Dan Gopstein <dan@gopstein.com>
2016-02-06 13:37:41 -0500

    2013 icpc publications

--- related_work/acm_toc_csv.rb
+++ related_work/acm_toc_csv.rb
@@ -5,22 +5,30 @@ require 'open-uri'
 require 'nokogiri'
 require 'csv'
 
-toc_url = 'icpc2014.html'
+toc_url = 'icpc2013.html'
 toc = Nokogiri::HTML(open(toc_url))
 
 XPATHS_ACM = {
   title: '//td/span/a[starts-with(@href, "citation")]/text()',
-  author: ['//ancestor::span[a[starts-with(@href, "author_page")]]', ->(s){s.gsub(/\s+/, ' ').gsub(/^ | $/, '')}],
-  link: ['//span[starts-with(text(), "Pages")]/following::tr[1]/td//a/@href', ->(s){"http://dl.acm.org/" + s}], # will grab the next link if the current one isn't there
-  abstract: '//span[starts-with(@id, "toHide")]',
+  author: ['//ancestor::span[a[starts-with(@href, "author_page")]]/text()', ->(s){s.gsub(/\s+/, ' ').gsub(/^ | $/, '')}],
+  link: ['//span[starts-with(text(), "Pages")]/following::tr[1]/td//a/@href/text()', ->(s){"http://dl.acm.org/" + s}], # will grab the next link if the current one isn't there
+  abstract: '//span[starts-with(@id, "toHide")]/text()',
 }
 
-XPATHS = XPATHS_ACM
+XPATHS_COMPORG = {
+  title: '//div[@class="tableOfContentsLineItemTitle"]/a[contains(@href, ".html")]/text()',
+  author: ['//div[@class="tableOfContentsLineItemTitle"]/a[contains(@href, ".html")]/ancestor::div[@class="tableOfContentsLineItem"]',
+          ->(x){x.xpath('./div[@class="tableOfContentsLineItemCreator"]//a').map(&:text).join(", ").gsub('"', '')}],
+  link: ['//input[starts-with(@name, "xplore_doi")]/@value', ->(x){'http://ieeexplore.ieee.org/xpl/articleDetails.jsp?arnumber='+x.text.split('.').last}],
+  #abstract: '',
+}
+
+XPATHS = XPATHS_COMPORG
 
 length = 43
 
 data = XPATHS.shallow_map_values do |col, (xpath, cleanup)| 
-  res = toc.xpath(xpath).map(&:text)
+  res = toc.xpath(xpath)
   if cleanup then res.map(&cleanup) else res end
 end
 
