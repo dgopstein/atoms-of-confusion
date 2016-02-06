ec4029a545ccc10e9dcb4a5195af1fe8a76ff826
Dan Gopstein <dan@gopstein.com>
2016-02-06 12:13:31 -0500

    Add acm icpc 2014 data

--- related_work/acm_toc_csv.rb
+++ related_work/acm_toc_csv.rb
@@ -5,17 +5,19 @@ require 'open-uri'
 require 'nokogiri'
 require 'csv'
 
-toc_url = 'icpc2015.html'
+toc_url = 'icpc2014.html'
 toc = Nokogiri::HTML(open(toc_url))
 
-XPATHS = {
+XPATHS_ACM = {
   title: '//td/span/a[starts-with(@href, "citation")]/text()',
   author: ['//ancestor::span[a[starts-with(@href, "author_page")]]', ->(s){s.gsub(/\s+/, ' ').gsub(/^ | $/, '')}],
   link: ['//span[starts-with(text(), "Pages")]/following::tr[1]/td//a/@href', ->(s){"http://dl.acm.org/" + s}], # will grab the next link if the current one isn't there
   abstract: '//span[starts-with(@id, "toHide")]',
 }
 
-length = 39
+XPATHS = XPATHS_ACM
+
+length = 43
 
 data = XPATHS.shallow_map_values do |col, (xpath, cleanup)| 
   res = toc.xpath(xpath).map(&:text)
