9ba2751f301097e17109b1fae9b14da86e9a2e54
Dan Gopstein <dan@gopstein.com>
2016-02-12 20:28:51 -0500

    true-ratio vs distance

--- snippet_study/priming.sql
+++ snippet_study/priming.sql
@@ -13,29 +13,42 @@
 --join usercode uc2 on uc.userid = uc2.userid and uc2.codeid = c.pair
 --where c.type = 'Confusing' and uc.userid = 1 limit 10;
 
-select uc2.timestamp < uc.timestamp, uc.correct, count(*)
--- ,t.tag -- By Atom
-from usercode uc
-join code c on uc.codeid = c.id
-join usercode uc2 on uc.userid = uc2.userid and uc2.codeid = c.pair
-join codetags ct on c.id = ct.codeid join tag t on ct.tagid = t.id
-where c.type = 'Confusing'
-group by uc2.timestamp < uc.timestamp, uc.correct
--- ,ct.tagid -- By Atom
-;
+--select uc2.timestamp < uc.timestamp, uc.correct, count(*)
+---- ,t.tag -- By Atom
+--from usercode uc
+--join code c on uc.codeid = c.id
+--join usercode uc2 on uc.userid = uc2.userid and uc2.codeid = c.pair
+--join codetags ct on c.id = ct.codeid join tag t on ct.tagid = t.id
+--where c.type = 'Confusing'
+--group by uc2.timestamp < uc.timestamp, uc.correct
+---- ,ct.tagid -- By Atom
+--;
 
+
+--.mode csv
+--select count(*), correct, (s.r2 - s.r1) / 5 as distance from (
+--  select uc.userid, uc.codeid, uc.correct --, uc.timestamp
+--  , (SELECT count(*) r FROM usercode t WHERE t.timestamp < uc.timestamp) r1
+--  , (SELECT count(*) r FROM usercode t WHERE t.timestamp < uc2.timestamp) r2
+--  from usercode uc
+--  join code c on uc.codeid = c.id
+--  join usercode uc2 on uc.userid = uc2.userid and uc2.codeid = c.pair
+--  join codetags ct on c.id = ct.codeid join tag t on ct.tagid = t.id
+--  where c.type = 'Confusing' order by r1
+--) s
+--GROUP BY correct, distance
+--order by distance
+--;
 
 .mode csv
-select count(*), correct, (s.r2 - s.r1) / 5 as distance from (
-  select uc.userid, uc.codeid, uc.correct --, uc.timestamp
-  , (SELECT count(*) r FROM usercode t WHERE t.timestamp < uc.timestamp) r1
-  , (SELECT count(*) r FROM usercode t WHERE t.timestamp < uc2.timestamp) r2
+select (uc2.rank - uc.rank) as distance,
+SUM(CASE WHEN uc.correct = 'T' THEN 1 ELSE 0 END) as Ts,
+SUM(CASE WHEN uc.correct = 'F' THEN 1 ELSE 0 END) as Fs
   from usercode uc
   join code c on uc.codeid = c.id
   join usercode uc2 on uc.userid = uc2.userid and uc2.codeid = c.pair
   join codetags ct on c.id = ct.codeid join tag t on ct.tagid = t.id
-  where c.type = 'Confusing' order by r1
-) s
-GROUP BY correct, distance
+  where c.type = 'Confusing'
+GROUP BY distance
 order by distance
 ;
