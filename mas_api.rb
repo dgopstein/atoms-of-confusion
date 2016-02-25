37fbc9b605f7d934dc75db87e4b3c3e6d763eade
Dan Gopstein <dan@gopstein.com>
2016-02-24 20:16:20 -0500

    paper title to id

--- mas_api.rb
+++ mas_api.rb
@@ -1,4 +1,6 @@
 require 'open-uri'
+require 'json'
+require 'deep_enumerable'
 
 # Microsoft Academic Search API
 class MasJsonApi
@@ -8,22 +10,37 @@ class MasJsonApi
     @key = key
   end
 
+  def parse_json(s)
+    JSON::parse(s)
+  end
+
   def url
-    BASE_URL + "?AppId=#@key&ResultObjects=publication&PublicationContent=title,author"
+    BASE_URL + "?AppId=#@key&ResultObjects=publication&PublicationContent=title,author" + "&StartIdx=1&EndIdx=2"
   end
 
-  def query(type, q)
-    query_url = "#{url}&#{type}=#{q}"
-    open(query_url).read
+  def query_by_title(title)
+    query_url = "#{url}&FullTextQuery=#{title}"
+    parse_json(open(query_url).read)
   end
 
-  def full_text_query(q)
-    query('FullTextQuery', q)
+  def dg(h, a)
+    h.deep_get(DeepEnumerable.deep_key_from_array(a))
+  end
+
+  def title_to_id(title)
+    response = query_by_title(title)
+    results = dg(response, ['d', 'Publication', 'Result'])
+    results.map{|r| r['ID']}
+  end
+
+  def get_citations(pub_id)
+    query_url = "http://academic.research.microsoft.com/json.svc/search?AppId=Your_AppID&PublicationID=#{pub_id}&ResultObjects=Publication&ReferenceType=Citation&StartIdx=1&EndIdx=10&OrderBy=Year"
+    open(query_url.read)
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
   key = open('MAS_API_KEY.txt').each_line.first.strip
   mja = MasJsonApi.new(key)
-  p mja.full_text_query('data mining')
+  p mja.title_to_id('The effectiveness of source code obfuscation: An experimental assessment.')
 end
