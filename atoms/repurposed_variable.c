2ff28d910ab013e473af5a8173a2f04a95089594
Dan Gopstein <dan@gopstein.com>
2016-02-25 23:53:17 -0500

    Repurposed variable

---
+++ atoms/repurposed_variable.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+
+// Confusing: array as variable
+void main1() {
+  int V1[5];
+  V1[4] = 4;
+
+  while (V1[4]) {
+    V1[4 - V1[4]] = V1[4];
+    V1[4] = V1[4] - 1;
+  }
+
+  printf("%d %d\n", V1[1], V1[4]);
+}
+
+// Non-Confusing: array as variable
+void main2() {
+  int V1[4];
+  int V2 = 4;
+
+  while (V2) {
+    V1[4 - V2] = V2;
+    V2 = V2 - 1;
+  }
+
+  printf("%d %d\n", V1[1], V2);
+}
+
+// Confusing: reusing incrementer
+void main3() {
+  for (int V1 = 0; V1 < 2; V1++) {
+    for (int V2 = 0; V1 < 2; V1++) {
+      printf("%d %d\n", V1, V2);
+    }
+  }
+}
+
+// Non-Confusing: reusing incrementer
+void main4() {
+  for (int V1 = 0; V1 < 2; V1++) {
+    for (int V2 = 0; V2 < 2; V2++) {
+      printf("%d %d\n", V1, V2);
+      V1 = V2;
+    }
+  }
+}
+
+// Confusing: mixing conditional and ordinal
+void main5() {
+  int V1;
+  for (int V2 = 0; V2 < 2; V2++) {
+    V1 = (V2 < 1);
+    if (V1) {
+      V1 = V2 + 5;
+    } else {
+      V1 = V1 + 2;
+    }
+  }
+  printf("%d\n", V1);
+}
+
+// Non-Confusing: mixing conditional and ordinal
+void main6() {
+  int V1;
+  for (int V2 = 0; V2 < 2; V2++) {
+    int V3 = (V2 < 1);
+    if (V3) {
+      V1 = V2 + 5;
+    } else {
+      V1 = V3 + 2;
+    }
+  }
+  printf("%d\n", V1);
+}
+
+int main() {
+  main1();
+  main2();
+
+  main3();
+  main4();
+
+  main5();
+  main6();
+}
