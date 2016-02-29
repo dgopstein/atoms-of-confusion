18be7461b57915f6c2b2a42f7b6fd0e5bbfec6a3
Dan Gopstein <dan@gopstein.com>
2016-02-29 12:55:55 -0500

    Curly braces

--- atoms/curly_braces.c
+++ atoms/curly_braces.c
@@ -23,11 +23,63 @@ void main2() {
   printf("%d\n", V1);
 }
 
-// Confusing: loop
-// Confusing: array initialization
+//Confusing: While Loop
+void main3() {
+  int V1 = 4;
+
+  int V2 = 0;
+  while (V2 < 3)
+    V2++;
+  V1++;
+
+  printf("%d %d\n", V1, V2);
+}
+
+//Non-Confusing: While Loop
+void main4() {
+  int V1 = 4;
+
+  int V2 = 0;
+  while (V2 < 3) {
+    V2++;
+  }
+  V1++;
+
+  printf("%d %d\n", V1, V2);
+}
+
+//Confusing: For Loop
+void main5() {
+  int V1 = 3;
+
+  for (int V2 = 0; V2 < 3; V2++)
+    V1++;
+  V1++;
+
+  printf("%d\n", V1);
+}
+
+//Non-Confusing: For Loop
+void main6() {
+  int V1 = 3;
+
+  for (int V2 = 0; V2 < 3; V2++) {
+    V1++;
+  }
+  V1++;
+
+  printf("%d\n", V1);
+}
+
 // Confusing: function declaration
 
 int main() {
   main1();
   main2();
+
+  main3();
+  main4();
+
+  main5();
+  main6();
 }
