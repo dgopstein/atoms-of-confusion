59fcab1137917815bfd7bf54e63cbac9b1786de6
Dan Gopstein <dan@gopstein.com>
2016-03-01 11:33:24 -0500

    Empty if

--- atoms/indentation.c
+++ atoms/indentation.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
+// Confusing: empty if
+void main1() {
+  int V1 = 0;
+  int V2 = 2;
+
+  if (V1);
+    V2 = 4;
+
+  printf("%d\n", V2);
+}
+
+// Non-Confusing: empty if
+void main2() {
+  int V1 = 0;
+  int V2 = 2;
+
+  if (V1);
+    V2 = 4;
+
+  printf("%d\n", V2);
+}
+
 int main() {
-  if (1 + 2 == 4);
-    printf("3 == 4\n");
+  main1();
+  main2();
 }
