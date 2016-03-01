aa42cb177130612accd4201651545d4d7ffaf7e4
Dan Gopstein <dan@gopstein.com>
2016-03-01 11:03:50 -0500

    type conversions

--- atoms/type_conversion.c
+++ atoms/type_conversion.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 //Confusing: float to int
-int main1() {
-  float V1 = 1.23;
+void main1() {
+  float V1 = 1.99;
 
   int V2 = V1;
 
@@ -10,16 +10,78 @@ int main1() {
 }
 
 //Non-Confusing: float to int
-int main2() {
-  int V1 = 1???;
+#include <math.h>
+void main2() {
+  float V1 = 1.99;
 
-  printf("%d\n", V1);
+  int V2 = trunc(1.99);
+
+  printf("%d\n", V2);
 }
 
 // Confusing: signed to unsigned
+void main3() {
+  int V1 = -1;
+
+  unsigned int V2 = V1;
+
+  int V3;
+  if (V2 > 0) {
+    V3 = 4;
+  } else {
+    V3 = 5;
+  }
+
+  printf("%d\n", V3);
+}
+
+// Non-Confusing: signed to unsigned
+#include <limits.h>
+void main4() {
+  int V1 = -1;
+
+  unsigned int V2;
+  if (V1 >= 0) {
+    V2 = V1;
+  } else {
+    V2 = UINT_MAX - V1;
+  }
+
+  int V3;
+  if (V2 >= 0) {
+    V3 = 4;
+  } else {
+    V3 = 5;
+  }
+
+  printf("%d\n", V3);
+}
 
 // Confusing: int to char
+void main5() {
+  int V1 = 261;
 
-void main() {
-  
+  char V2 = V1;
+
+  printf("%d\n", V2);
+}
+
+// Confusing: int to char
+void main6() {
+  int V1 = 261;
+
+  char V2 = V1 % 256;
+
+  printf("%d\n", V2);
+}
+
+int main() {
+  main1();
+  main2();
+
+  main3();
+  main4();
+
+  main5();
+  main6();
 }
