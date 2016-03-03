a1236d837e331c68b875a4527d69ffcfabe2ee6e
Dan Gopstein <dan@gopstein.com>
2016-03-02 22:20:48 -0500

    More indentation atoms

--- atoms/indentation.c
+++ atoms/indentation.c
@@ -5,7 +5,7 @@ void main1() {
   int V1 = 0;
   int V2 = 2;
 
-  if (V1);
+  if (V1) {}
     V2 = 4;
 
   printf("%d\n", V2);
@@ -16,13 +16,69 @@ void main2() {
   int V1 = 0;
   int V2 = 2;
 
-  if (V1);
-    V2 = 4;
+  if (V1) {}
+  V2 = 4;
 
   printf("%d\n", V2);
+}
+
+// Confusing: full if
+void main3() {
+  int V1 = 0;
+  int V2 = 1;
+
+  if (V1) {
+    V2 = 2;
+  }
+    V2 = V2 * 3;
+
+  printf("%d\n", V2);
+}
+
+// Non-Confusing: full if
+void main4() {
+  int V1 = 0;
+  int V2 = 1;
+
+  if (V1) {
+    V2 = 2;
+  }
+  V2 = V2 * 3;
+
+  printf("%d\n", V2);
+}
+
+// Confusing: while
+void main5() {
+  int V1 = 0;
+
+  for (int V2 = 0; V2 < 3; V2++) {
+    V1 = V1 + 2;
+  }
+    V1 = V1 + 1;
+
+  printf("%d\n", V1);
+}
+
+// Non-Confusing: while
+void main6() {
+  int V1 = 0;
+
+  for (int V2 = 0; V2 < 3; V2++) {
+    V1 = V1 + 2;
+  }
+  V1 = V1 + 1;
+
+  printf("%d\n", V1);
 }
 
 int main() {
   main1();
   main2();
+
+  main3();
+  main4();
+
+  main5();
+  main6();
 }
