7e276bf479a7fc228e23cc0d6a6df26a2a638e85
Dan Gopstein <dan@gopstein.com>
2016-04-13 16:38:55 -0400

    irreconsilable undefined behavior

--- simplifications/anonymous_1984/anonymous.c
+++ simplifications/anonymous_1984/anonymous.c
@@ -1,2 +1,3 @@
-int i;main(){for(;i["]<i;++i){--i;}"];read('-'-'-',i+++"hell\
-o, world!\n",'/'/'/'));}read(j,i,p){write(j/p+p,i---j,i/i);}
+void read(int j, char *i, int p){write(j/p+p,i---j,i/i);}
+int i;main(){for(;i["]<i;++i){--i;}"]; read('-'-'-',i+++"hell\
+o, world!\n",'/'/'/'));}
