ce14d08c31cc5338010e73603feafee854dd22d3
Dan Gopstein <dan@gopstein.com>
2016-04-13 11:57:47 -0400

    anonymous 1984

---
+++ simplifications/anonymous_1984/anonymous_jake.c
@@ -0,0 +1,11 @@
+int i;
+main()
+{
+  for (; "]<i;++i){--i;}"[i] != '\0'; F1('-' - '-', "hello, world!\n"[i], '/' / '/'), i++);
+}
+F1(j, i, p)
+{
+  printf("-%d %d %d-\n", j / p + p, i - j, i / i);
+//  write(1, &"h", 1);
+  write(j / p + p, i - j, i / i), i--;
+}
