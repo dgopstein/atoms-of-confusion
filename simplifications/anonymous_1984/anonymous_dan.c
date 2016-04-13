87fe17990c0a1febd5d13fffd69ba332f5a95893
Dan Gopstein <dan@gopstein.com>
2016-04-13 12:22:33 -0400

    Weird behavior

--- simplifications/anonymous_1984/anonymous_dan.c
+++ simplifications/anonymous_1984/anonymous_dan.c
@@ -1,2 +1,30 @@
-int i;main(){for(;i["]<i;++i){--i;}"];read('-'-'-',i+++"hell\
-o, world!\n",'/'/'/'));}read(j,i,p){write(j/p+p,i---j,i/i);}
+#include <stdio.h>
+char* V1 = "hello, world!\n";
+int i;main(){for(;i["]<i;++i){--i;}"];F1('-'-'-',i+++V1
+,'/'/'/')) {
+  printf("i: %d\n", i);
+}
+}
+
+F1(j,k,p){
+  //printf("- %d, %d, %d -\n", j/p+p,k---j,k/k);
+  printf("%d %d\n", V1 + i - 1, k - j);
+  printf("equal: %d\n", V1 + i - 1 == k - j);
+
+  //printf("fake write: ["); fflush(stdout);
+  write(1,V1 + i - 1,k/k);
+
+  //printf("]\n"); printf("real write: ["); fflush(stdout);
+  //write(j/p+p,k---j,k/k);
+  write(j/p+p,k-j,k/k);
+
+
+
+  //printf("]\n");
+  //echo(j/p+p,k-j,k/k);
+  k--;
+}
+
+//echo(a, b, c) {
+//  printf("echo: %d, %d, %d", a, b, c);
+//}
