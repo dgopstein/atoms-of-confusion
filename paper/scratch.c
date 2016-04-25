1af032dfbe5d7bcfa2d17e8bb52d7d5884fa1370
Dan Gopstein <dan@gopstein.com>
2016-04-25 10:35:39 -0400

    imprecision in floats

--- paper/scratch.c
+++ paper/scratch.c
@@ -1,22 +1,26 @@
+#include <stdio.h>
+
 int main() {
   //int i = 123;
   //int j = 45 + i++;
 
-  int x = 2, y = 3;
-
-  x = ++y;
+//  int x = 2, y = 3;
+//
+//  x = ++y;
 
 //  int d = &((void*)12)[34];
 //  printf("46: %d\n", d);
 //
 
-  x = 1.0 / 10;
 
-  int y;
-  for (int i = 0; i < 10; i++) {
-    y = x * 10;
-  }
+  //for (int i = 0; i < 10; i++) {
+  //  b = a * (i + 1);
+  //}
 
-  printf("y: %d\n");
+  float a = 1.1 / 10.0;
+  float b = a * 10;
+
+  printf("b: %d\n", b == 1.1);
+  //printf("b: %d\n", 10*(1.1 / 10.0) == 1.1);
 
 }
--- paper/scratch.s
+++ paper/scratch.s
@@ -1,5 +1,10 @@
 	.section	__TEXT,__text,regular,pure_instructions
 	.macosx_version_min 10, 11
+	.section	__TEXT,__literal4,4byte_literals
+	.align	2
+LCPI0_0:
+	.long	1038323257              ## float 0.111111112
+	.section	__TEXT,__text,regular,pure_instructions
 	.globl	_main
 	.align	4, 0x90
 _main:                                  ## @main
@@ -13,16 +18,42 @@ Ltmp1:
 	movq	%rsp, %rbp
 Ltmp2:
 	.cfi_def_cfa_register %rbp
-	xorl	%eax, %eax
-	movl	$2, -4(%rbp)
-	movl	$3, -8(%rbp)
-	movl	-8(%rbp), %ecx
-	addl	$1, %ecx
-	movl	%ecx, -8(%rbp)
-	movl	%ecx, -4(%rbp)
+	subq	$32, %rsp
+	movss	LCPI0_0(%rip), %xmm0    ## xmm0 = mem[0],zero,zero,zero
+	movl	$0, -4(%rbp)
+	movss	%xmm0, -12(%rbp)
+	movl	$0, -16(%rbp)
+LBB0_1:                                 ## =>This Inner Loop Header: Depth=1
+	cmpl	$9, -16(%rbp)
+	jge	LBB0_4
+## BB#2:                                ##   in Loop: Header=BB0_1 Depth=1
+	movss	-12(%rbp), %xmm0        ## xmm0 = mem[0],zero,zero,zero
+	movl	-16(%rbp), %eax
+	addl	$1, %eax
+	cvtsi2ssl	%eax, %xmm1
+	mulss	%xmm1, %xmm0
+	movss	%xmm0, -8(%rbp)
+## BB#3:                                ##   in Loop: Header=BB0_1 Depth=1
+	movl	-16(%rbp), %eax
+	addl	$1, %eax
+	movl	%eax, -16(%rbp)
+	jmp	LBB0_1
+LBB0_4:
+	leaq	L_.str(%rip), %rdi
+	cvtss2sd	-8(%rbp), %xmm0
+	movb	$1, %al
+	callq	_printf
+	movl	-4(%rbp), %ecx
+	movl	%eax, -20(%rbp)         ## 4-byte Spill
+	movl	%ecx, %eax
+	addq	$32, %rsp
 	popq	%rbp
 	retq
 	.cfi_endproc
+
+	.section	__TEXT,__cstring,cstring_literals
+L_.str:                                 ## @.str
+	.asciz	"b: %f\n"
 
 
 .subsections_via_symbols
