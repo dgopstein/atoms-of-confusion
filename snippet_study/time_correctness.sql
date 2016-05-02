ff7afa4b523c76f80c4bd832dee5a3592e1c22da
Dan Gopstein <dan@gopstein.com>
2016-05-02 15:53:44 -0400

    time correcness sql

---
+++ snippet_study/time_correctness.sql
@@ -0,0 +1,11 @@
+select t.tag||'_'||uc.codeid, c.type,  
+SUM(CASE WHEN uc.correct = 'T' THEN 1 ELSE 0 END) as correct,
+SUM(CASE WHEN uc.correct = 'F' THEN 1 ELSE 0 END) as incorrect,
+SUM(CASE WHEN uc.correct = 'T' THEN 1.0 ELSE 0.0 END) /
+max(SUM(CASE WHEN uc.correct = 'F' THEN 1.0 ELSE 0.0 END), 1.0) as ratio,
+avg(uc.duration)
+from usercode uc
+join code c on uc.codeid = c.id
+join codetags ct on ct.codeid = c.id
+join tag t on ct.tagid = t.id
+group by ct.codeid;
