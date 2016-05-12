f7d206a60c4260f9d88baa7402a32aef3c144c82
Dan Gopstein <dan@gopstein.com>
2016-05-12 14:46:57 -0400

    Revert "No clue what's happening"
    This reverts commit 81122e0dee3467a47f1682d6ba5af0fe3a244bf0.

--- snippet_study/results.R
+++ snippet_study/results.R
@@ -3,8 +3,6 @@ library("fitdistrplus")
 library(DBI)
 library("data.table")
 library('plyr')
-library('dplyr')
-library('functional')
 
 
 # add += operator
@@ -17,21 +15,12 @@ alltables <- dbListTables(con)
 contingencyQuery <- paste(readLines('contingency.sql'), collapse = "\n")
 
 contingencies <- dbGetQuery( con, contingencyQuery )
-#contingenciesFrame <- ldply(contingencies, data.frame)
-contingenciesFrame <- do.call(rbind.data.frame, contingencies)
-#print(contingenciesFrame)
-#print(contingencies)
-#exit
-
-
-alpha <- 0.05
 
 phi <- function(chi2, n) {
   sqrt(chi2/n)
 }
 
-printContingency <- function(name, res, contingency) {
-
+printContingency <- function(name, alpha, res, contingency) {
   sig <- is.significant(res, alpha)
   es <- phi(res$statistic, sum(contingency))
   contStr <- paste(format(contingency, width=3), collapse=" ")
@@ -49,15 +38,17 @@ is.significant <- function(res, alpha) {
 
 byQuestion <- function(contingencies) {
   sigCounts <- list()
-
-  testQuestion <- function (questionCont) {
-    writeLines(str(questionCont))
-    writeLines(paste("qc$TT: ", str(questionCont$TT)))
+  for (question in contingencies$question) {
+    questionCont <- contingencies[contingencies$question == question,]
     
     contingency <- matrix(c(questionCont$TT,  questionCont$TF, questionCont$FT,questionCont$FF), 2, 2)
     res <- mcnemar.test(contingency, correct=FALSE)
 
-    printContingency(questionCont$question, res, contingency)
+    #es <- assocstats(contingency)
+
+    alpha <- 0.05
+
+    printContingency(question, alpha, res, contingency)
 
     if (!(questionCont$atom %in% names(sigCounts))) {
       sigCounts[[questionCont$atom]] <- 0
@@ -67,27 +58,9 @@ byQuestion <- function(contingencies) {
     }
   }
 
-  #for (question in contingencies$question) {
-  #  questionCont <- contingencies[contingencies$question == question,]
-  ##for (questionCont in contingencies) {
-  #  testQuestion(questionCont)
-  #}
-  #writeLines(str(contingencies))
-
-  #apply(contingencies, 1, Compose(print, str))
-  c1 <- contingencies[1]
-  writeLines(colnames(c1))
-  matrices <- apply(contingencies, 1, function(x) matrix(c(x['TT'], x['TF'], x['FT'], x['FF']), 2, 2))
-
-  print(str(matrices[1]))
-
-  lapply(contingencies, testQuestion)
-
-  #sigCounts2 <- sigCounts %>% group_by()
-
-  lapply(names(sigCounts), function(a) writeLines(sprintf("counts %d - %s", sigCounts[[a]], a)))
-
-  sigCounts
+  for (atom in names(sigCounts)) {
+    writeLines(sprintf("counts %d - %s", sigCounts[[atom]], atom))
+  }
 }
 
 processAtom <- function(atomName) {
@@ -107,7 +80,7 @@ processAtom <- function(atomName) {
     # effectSize <- assocstats(contingency)
     
     # cat("signtest: ")
-    # printContingency(atom, res, contingency)
+    # printContingency(atom, alpha, res, contingency)
 
     list('atomName' = atomName, 'contingency' = contingency, 'mcnemarsRes' = mcnemarsRes)
 }
@@ -116,20 +89,18 @@ byAtom <- function(contingencies) {
   writeLines(sprintf("%-35s: sig. - (pvalue, effectSize)  (TT TF FT FF)", "atom"))
   
   # There are 3 questions for each atom
+  alpha <- 0.05
+  # for (atom in unique(contingencies$atom)) {
+  #   processRes <- processAtom(atom)
+
   atomRes <- lapply(unique(contingencies$atom), processAtom)
 
-  lapply(atomRes, function (r) printContingency(r$atomName, r$mcnemarsRes, r$contingency))
+  lapply(atomRes, function (r) printContingency(r$atomName, alpha, r$mcnemarsRes, r$contingency))
 
-  atomRes
+  NULL
 }
 
-aggQuestions <- function(questionCounts) {
-  
-}
 
-questionCounts <- byQuestion(contingenciesFrame)
+byQuestion(contingencies)
 
-questionAgg <- aggQuestions(questionCounts)
-
-atomRes <- byAtom(contingencies)
-
+byAtom(contingencies)
