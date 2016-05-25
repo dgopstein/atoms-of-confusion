6eb00a07b0182838e732e99c072c191f41f48018
Dan Gopstein <dan@gopstein.com>
2016-05-24 20:00:58 -0400

    remove obuchowski closure dependencies

--- snippet_study/obuchowski.R
+++ snippet_study/obuchowski.R
@@ -24,30 +24,25 @@ FT <- cnts$FT
 # I = number of treatments (2, confusing/non-confusing)
 # x_ij = number of questions that responded to treatment (TF/FT)
 
-m <- length(unique(cnts$atom))
+obuchowski.test <- function(x1, x2, N, clusters) {
+  m <- length(unique(clusters))
+  ((p.hat(x1, N) - p.hat(x2, N))^2) / var.hat.diff(x1, x2, N, m)
+}
 
 # Eqn (1)
-p.hat <- function(x) sum(x) / sum(N)
+p.hat <- function(x, N) sum(x) / sum(N)
 
 # Eqn (2)
-var.hat <- function(x)
-  m * (m-1)^-1 * sum( (x - (N * p.hat(x)))^2 ) / sum(N)^2
+var.hat <- function(x, N, m)
+  m * (m-1)^-1 * sum( (x - (N * p.hat(x, N)))^2 ) / sum(N)^2
 
 # Eqn (3)
-cov.hat <- function(x, x1)
-  m * (m-1)^-1 * sum( (x - (N * p.hat(x))) * (x1 - (N * p.hat(x1))) ) / sum(N)^2
+cov.hat <- function(x, x1, N, m)
+  m * (m-1)^-1 * sum( (x - (N * p.hat(x, N))) * (x1 - (N * p.hat(x1, N))) ) / sum(N)^2
 
 # Eqn (4)
-var.hat.diff <- function(x, x1)
-  var.hat(x) + var.hat(x1) - 2 * cov.hat(x, x1)
-  
-obuchowski <- function (x1, x2)
-  ((p.hat(x1) - p.hat(x2))^2) / var.hat.diff(x1, x2)
+var.hat.diff <- function(x, x1, N, m)
+  var.hat(x, N, m) + var.hat(x1, N, m) - 2 * cov.hat(x, x1, N, m)
 
-obuchowski(TF, FT)
-
-((p.hat(TF) - p.hat(FT))^2)
-var.hat.diff(x1, x2)
-
-
-cnts[, .(chisq = obuchowski(FT, TF), sig = obuchowski(FT, TF) > qchisq(0.95, 1)), by=atom]
+chis <- cnts[, .(chisq = obuchowski.test(TF, FT, N, userId)), by=atom]
+chis[, .(sig = chisq > qchisq(0.95, 1))]
