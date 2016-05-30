b37e11c7aee77391f77ea15bd9ceef5027cd7850
Dan Gopstein <dan@gopstein.com>
2016-05-30 18:05:26 -0400

    Changed operator spacing

--- simplifications/endoh4-2015/decoded/0008-operator-spacing.c
+++ simplifications/endoh4-2015/decoded/0008-operator-spacing.c
@@ -3,9 +3,8 @@
 int F1(int V1, int V2) {
   int V3, V4;
 
-  V4 = 1;
-  for (V3 = V4;
-       V3* V3 <= V1 ? V4 = V1 % V3 ? V4 : V3 : V2 + 1 ? V4 < 2 ? V1 && F1(V2, 0) : F1(V4, V2),
+  for (V3 = V4 = 1;
+       V3 * V3 <= V1 ? V4 = V1 % V3 ? V4 : V3 : V2 + 1 ? V4 < 2 ? V1 && F1(V2, 0) : F1(V4, V2),
       printf("%c", V2 ? 10 : 32 << !V1), V1 -= V4 * !!V1 : (F1(V4, V1 / V4), 0);)
     V3++;
 
