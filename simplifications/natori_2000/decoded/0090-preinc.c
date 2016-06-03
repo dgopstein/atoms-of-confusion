6f6905340beb9f5cd5996be1be4b0c9a9d718e8d
Dan Gopstein <dan@gopstein.com>
2016-06-03 18:30:37 -0400

    done simplifying

--- simplifications/natori_2000/decoded/0090-preinc.c
+++ simplifications/natori_2000/decoded/0090-preinc.c
@@ -6,14 +6,13 @@ int main(int V1, char **V2, char **V3) {
 
   int V5 = V1;
   V1 = V1 - 1;
-  if (V5 + 1 != 0 && V5 + 4 != 0) {
+  if (V5 + 1 && V5 + 4) {
     main(V1, -8, V1);
   }
 
   if (V1 && V2) {
-    V2 = V2 + 1;
-    main(-1, V2, V3);
-    l = trunc((int)(V2 + 1) / (1 - ((int)V3 * 2) - ((int)V3 * (int)V3)));
+    main(-1, ++V2, V3);
+    l = (int)(V2 + 1) / (1 - (int)V3 * 2 - (int)V3 * (int)V3);
     V4 = "ab"[((l * l) > 3) && (((trunc((2 % 3) / 4) - 2) + (l / 2)) < 1)];
   } else {
     V4 = 'c';
--- simplifications/natori_2000/decoded/0100-implicit-condition.c
+++ simplifications/natori_2000/decoded/0100-implicit-condition.c
@@ -11,9 +11,8 @@ int main(int V1, char **V2, char **V3) {
   }
 
   if (V1 && V2) {
-    V2 = V2 + 1;
-    main(-1, V2, V3);
-    l = trunc((int)(V2 + 1) / (1 - ((int)V3 * 2) - ((int)V3 * (int)V3)));
+    main(-1, ++V2, V3);
+    l = (int)(V2 + 1) / (1 - (int)V3 * 2 - (int)V3 * (int)V3);
     V4 = "ab"[((l * l) > 3) && (((trunc((2 % 3) / 4) - 2) + (l / 2)) < 1)];
   } else {
     V4 = 'c';
