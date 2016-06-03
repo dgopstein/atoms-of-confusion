0322fe8a0e4453c8ce7830ad8f62baa8ebd9d03f
Dan Gopstein <dan@gopstein.com>
2016-06-03 17:53:41 -0400

    transformations

--- simplifications/natori_2000/decoded/0040-swap-subscripts.c
+++ simplifications/natori_2000/decoded/0040-swap-subscripts.c
@@ -1,4 +1,3 @@
-#include <math.h>
 #include <stdio.h>
 double l;
 int main(int V1, char **V2, char **V3) {
@@ -7,7 +6,7 @@ int main(int V1, char **V2, char **V3) {
   if (V1-- + 1 && V1 + 4 && main(V1, -8, V1), V1 && V2) {
     main(-1, ++V2, V3);
     l = (int)(V2 + 1) / (1 - (int)V3 * 2 - (int)V3 * (int)V3);
-    V4 = "ab"[((l * l) > 3) && (((trunc((2 % 3) / 4) - 2) + (l / 2)) < 1)];
+    V4 = "ab"[l * l > 3 && ((2 % 3) / 4 - 2 + (l / 2)) < 1];
   } else {
     V4 = 'c';
   }
