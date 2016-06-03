0942a988178cdf558e92c92d39607ac6fd677d2e
Dan Gopstein <dan@gopstein.com>
2016-06-03 09:09:32 -0400

    Finish normalization and add test

---
+++ simplifications/natori_2000/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+gcc="gcc -w"
+
+src_a=natori.c
+src_b=decoded/0005*
+
+prg_a=`basename $src_a`
+prg_b=`basename $src_b`
+prg_a="bin/${prg_a%.*}"
+prg_b="bin/${prg_b%.*}"
+
+$gcc $src_a -o $prg_a || exit 1
+$gcc $src_b -o $prg_b || exit 2
+
+cmp  <($prg_a) <($prg_b)
+if [[ 0 -ne $? ]]; then
+  echo "Failure"
+else
+  echo "Success"
+fi
