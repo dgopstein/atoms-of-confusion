599e895f1febc09404661576979cf48ea0a942a0
Dan Gopstein <dan@gopstein.com>
2016-06-05 19:30:22 -0400

    Recon variable names l->V4

--- simplifications/2000-natori/decoded/2-simplify-040-swap-subscripts.c
+++ simplifications/2000-natori/decoded/2-simplify-040-swap-subscripts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-double l;
+double V4;
 int main(int V1, char **V2, char **V3) {
   char V4;
 
   if (V1-- + 1 && V1 + 4 && main(V1, -8, V1), V1 && V2) {
     main(-1, ++V2, V3);
-    l = (int)(V2 + 1) / (1 - (int)V3 * 2 - (int)V3 * (int)V3);
-    V4 = "ab"[l * l > 3 && ((2 % 3) / 4 - 2 + (l / 2)) < 1];
+    V4 = (int)(V2 + 1) / (1 - (int)V3 * 2 - (int)V3 * (int)V3);
+    V4 = "ab"[V4 * V4 > 3 && ((2 % 3) / 4 - 2 + (V4 / 2)) < 1];
   } else {
     V4 = 'c';
   }
--- simplifications/2000-natori/decoded/2-simplify-100-implicit-condition.c
+++ simplifications/2000-natori/decoded/2-simplify-100-implicit-condition.c
@@ -1,6 +1,6 @@
 #include <math.h>
 #include <stdio.h>
-double l;
+double V4;
 int main(int V1, char **V2, char **V3) {
   char V4;
 
@@ -12,8 +12,8 @@ int main(int V1, char **V2, char **V3) {
 
   if (V1 && V2) {
     main(-1, ++V2, V3);
-    l = (int)(V2 + 1) / (1 - (int)V3 * 2 - (int)V3 * (int)V3);
-    V4 = "ab"[((l * l) > 3) && (((trunc((2 % 3) / 4) - 2) + (l / 2)) < 1)];
+    V4 = (int)(V2 + 1) / (1 - (int)V3 * 2 - (int)V3 * (int)V3);
+    V4 = "ab"[((V4 * V4) > 3) && (((trunc((2 % 3) / 4) - 2) + (V4 / 2)) < 1)];
   } else {
     V4 = 'c';
   }
