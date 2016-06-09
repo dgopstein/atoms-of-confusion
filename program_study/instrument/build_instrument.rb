6881b22da28cfa567607513160c33dc3d219b348
Dan Gopstein <dan@gopstein.com>
2016-06-09 01:51:40 -0400

    read from file

--- program_study/instrument/build_instrument.rb
+++ program_study/instrument/build_instrument.rb
@@ -2,32 +2,44 @@
 
 require 'mustache'
 
-if ARGV.length != 2
+if ARGV.length == 1
+  infile = ARGV.first
+  subjects = File.read(infile).lines
+  subjects.each do |line|
+    subject_id, question_order = line.split(/s+,?s+/)
+    write_pdf(subject_id, question_order)
+  end
+elsif ARGV.length == 2
+  subject_id, question_order = ARGV
+  write_pdf(subject_id, question_order)
+else
+  puts "Usage: build_instrument.rb subject_file.csv"
   puts "Usage: build_instrument.rb subject_id question_order"
   exit 1
 end
 
-subject_id, question_order = ARGV
 
-questions = question_order.split('')
-
-intro_tex = Mustache.render(File.read("instrument-intro.mustache"), subject_id: subject_id)
-
-questions_tex = questions.map do |q_letter|
-  question_file = q_letter.downcase + '.c'
-  question_name = q_letter.upcase
-
-  Mustache.render(File.read("instrument-question.mustache"),
-    question_file: question_file,
-    question_name: question_name)
+def write_pdf(subject_id, question_order)
+  questions = question_order.split('')
+  
+  intro_tex = Mustache.render(File.read("instrument-intro.mustache"), subject_id: subject_id)
+  
+  questions_tex = questions.map do |q_letter|
+    question_file = q_letter.downcase + '.c'
+    question_name = q_letter.upcase
+  
+    Mustache.render(File.read("instrument-question.mustache"),
+      question_file: question_file,
+      question_name: question_name)
+  end
+  
+  survey_tex = File.read("instrument-survey.mustache")
+  
+  tex = [intro_tex, questions_tex, survey_tex].flatten.join
+  
+  filename = "out/instrument-#{subject_id}.tex"
+  
+  File.write(filename, tex)
+  
+  `pdflatex --output-directory=out/ #{filename}`
 end
-
-survey_tex = File.read("instrument-survey.mustache")
-
-tex = [intro_tex, questions_tex, survey_tex].flatten.join
-
-filename = "out/instrument-#{subject_id}.tex"
-
-File.write(filename, tex)
-
-`pdflatex --output-directory=out/ #{filename}`
