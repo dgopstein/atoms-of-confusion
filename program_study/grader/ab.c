c77148351f412773ff26ed9ec70bc215fd683c93
Dan Gopstein <dan@gopstein.com>
2016-06-10 15:14:11 -0400

    More grader code

--- program_study/grader/ab.c
+++ program_study/grader/ab.c
@@ -11,17 +11,15 @@
       default: return; \
     }
 
-int in_V1, in_V3;
-char label, in_V2[100];
+int in_V1, in_V3, in_V4, in_V6;
+char label, in_V2[100], in_V5[100];
 
 void F1(int V1, char *V2, int V3) {
   // printf("a: %d %s %d\n", V1, V2, V3);
   {
     scanf("%c: %d %s %d", &label, &in_V1, in_V2, &in_V3);
 
-    printf("V1: %d - %d: %d\n", V1, in_V1, V1 == in_V1);
-    printf("V2: %s - %s: %d\n", V2, in_V2, !strcmp(V2, in_V2));
-    printf("V3: %d - %d: %d\n", V3, in_V3, V3 == in_V3);
+    printf("a-%d: V1-%d, V2-%d: V3-%d\n", 'a' == label,V1 == in_V1,  !strcmp(V2, in_V2), V3 == in_V3);
 
     GO(label);
 
@@ -34,9 +32,19 @@ void F1(int V1, char *V2, int V3) {
   char *V5 = V2 - V1;
   V2 = V2 - 1;
   int V6 = (int)V2 / (int)V2;
-  printf("b: %d %s %d\n", V4, V5, V6);
+  //printf("b: %d %s %d\n", V4, V5, V6);
   {
+    scanf("%c: %d %s %d", &label, &in_V4, in_V5, &in_V6);
+
+    printf("%c: %d %s %d\n", label, in_V4, in_V5, in_V6);
+    printf("b-%d: V4-%d, V5-%d: V6-%d\n", 'b' == label,V4 == in_V4,  !strcmp(V5, in_V5), V6 == in_V6);
+
+    GO(label);
+
     b:;
+    V4 = in_V4;
+    V5 = in_V5;
+    V6 = in_V6;
   }
 }
 int V7;
