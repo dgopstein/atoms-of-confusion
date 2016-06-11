a5bbf348950026afe25892ff31ab55ca9d707df9
Dan Gopstein <dan@gopstein.com>
2016-06-11 11:34:50 -0400

    correctly grades mostly correct data

--- program_study/grader/ab.c
+++ program_study/grader/ab.c
@@ -17,7 +17,7 @@ char label, in_V2[100], in_V5[100];
 void F1(int V1, char *V2, int V3) {
   // printf("a: %d %s %d\n", V1, V2, V3);
   {
-    scanf("%c: %d %s %d", &label, &in_V1, in_V2, &in_V3);
+    scanf(" %c: %d %s %d", &label, &in_V1, in_V2, &in_V3);
 
     printf("a-%d: V1-%d, V2-%d: V3-%d\n", 'a' == label,V1 == in_V1,  !strcmp(V2, in_V2), V3 == in_V3);
 
@@ -34,9 +34,8 @@ void F1(int V1, char *V2, int V3) {
   int V6 = (int)V2 / (int)V2;
   //printf("b: %d %s %d\n", V4, V5, V6);
   {
-    scanf("%c: %d %s %d", &label, &in_V4, in_V5, &in_V6);
+    scanf(" %c: %d %s %d", &label, &in_V4, in_V5, &in_V6);
 
-    printf("%c: %d %s %d\n", label, in_V4, in_V5, in_V6);
     printf("b-%d: V4-%d, V5-%d: V6-%d\n", 'b' == label,V4 == in_V4,  !strcmp(V5, in_V5), V6 == in_V6);
 
     GO(label);
