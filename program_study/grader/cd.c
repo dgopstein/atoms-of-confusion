c9dc31e372c03421a1070bf7abca30b06ff06e64
Dan Gopstein <dan@gopstein.com>
2016-06-13 11:55:50 -0400

    cd.c

---
+++ program_study/grader/cd.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+void F1(int V1, int V2, int V3, int V4) {
+  V1 = V1 + 1;
+  V2 = V1;
+  while (V2 < 4) {
+    V3 = 0;
+
+    printf("a: %d %d %d %d\n", V1, V2, V3, V4);
+    int V9;
+    if (V3 < V2) {
+      V3 = (V3 * 8) + (V2 % 8);
+      V2 /= 8;
+      V4 = (V3 == V2) | ((V3 / 8) == V2);
+      V9 = 1;
+    } else {
+      V4 = V4 + 1;
+      if ((V1 - V4) != 0) {
+        V9 = V1 % V4;
+      } else {
+        printf("b: %d\n", V1);
+        V9 = 2 && (V1 % V4);
+      }
+    }
+
+    for (; V9;) {
+      printf("c: %d %d %d %d\n", V1, V2, V3, V4);
+      if (V3 < V2) {
+        V3 = (V3 * 8) + (V2 % 8);
+        V2 /= 8;
+        V4 = (V3 == V2) | ((V3 / 8) == V2);
+        V9 = 1;
+      } else {
+        V4 = V4 + 1;
+        if ((V1 - V4) != 0) {
+          V9 = V1 % V4;
+        } else {
+          printf("d: %d\n", V1);
+          V9 = 2 && (V1 % V4);
+        }
+      }
+    }
+
+    V1 = V1 + 1;
+    V2 = V1;
+  }
+}
+
+int main() {
+  F1(1, 0, 0, 0);
+  printf("e\n");
+}
