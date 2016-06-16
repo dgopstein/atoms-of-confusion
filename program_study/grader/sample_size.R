4b8e732b004fa2571f5577012824310b4d62e759
Dan Gopstein <dan@gopstein.com>
2016-06-16 15:07:28 -0400

    don't use chi-sq

--- program_study/grader/sample_size.R
+++ program_study/grader/sample_size.R
@@ -19,31 +19,37 @@ abline(0,1)
 # sum.tss <- n.ttest(power = 0.8, alpha = 0.05, mean.diff = mean(c.sum) - mean(nc.sum), sd1 = sd(c.sum), sd2 = sd(nc.sum))
 # sum.wt <- wilcox.test
 
-
-scores <- function(char) pilot.results[qtype == char, correct/points]
-
 cs <- c('a', 'c', 'e', 'g')
 ncs <- c('b', 'd', 'f', 'h')
 
-w.vs.t <- function (as, bs, name = "") {
-  wt <- wilcox.test(as, bs, conf.int = TRUE)
+w.vs.t <- function (acounts, bcounts, name = "") {
+  as = acounts$correct / acounts$points
+  bs = bcounts$correct / bcounts$points
   
+  # Wilcoxon
+  wt <- wilcox.test(as, bs, conf.int = TRUE)
   wss <- n.wilcox.ord(power = 0.8, alpha = 0.05, t = 0.5, p = c(mean(as), 1 - mean(as)), q = c(mean(bs), 1 - mean(bs)))
   
+  # T.test
   tt <- t.test(as, bs)
   tss <- n.ttest(power = 0.8, alpha = 0.05, mean.diff = mean(as) - mean(bs), sd1 = sd(as), sd2 = sd(bs))
   cohens.d <- cohensD(as,bs)
   
-  #list("pair" =name, "t.p.value" = tt$p.value, "t.ss" = tss$`Total sample size`, "wilcox.p.value" = wt$p.value, "wilcox.ss" = wss$`total sample size`)
+  # Chi-Sq
+  # Chi-Sq is invalid because the data are independent
+  # cnt.table <- cbind(c = colSums(acounts), nc = colSums(bcounts))
+  # x2 <- chisq.test(cnt.table)
+
+  # Results
   res <- c(name, tt$p.value, tss$`Total sample size`, cohens.d, wt$p.value, wss$`total sample size`, wt$estimate)
   names(res) <- c("pair", "t.p.value", "t.ss", "cohens.d", "wilcox.p.value", "wilcox.ss", "wilcox.estimate")
-  
   res
 }
 
+
 pair.test <- function(a, b) {
-  as <- scores(a)
-  bs <- scores(b)
+  as <- pilot.results[qtype == a, .(correct, points)]
+  bs <- pilot.results[qtype == b, .(correct, points)]
   
   w.vs.t(as, bs, name =  paste(a, b, sep=""))
 }
@@ -53,10 +59,15 @@ pair.data <- t(mapply(pair.test, cs, ncs))
 print(pair.data)
 print(w.vs.t(c.sum, nc.sum))
 
-hist(c.sum, breaks = 10)
-hist(nc.sum, breaks = 10)
+# hist(c.sum, breaks = 10)
+# hist(nc.sum, breaks = 10)
+# 
+# qqnorm(c.sum)
+# qqnorm(nc.sum)
+# 
+# t.test(c.sum, nc.sum)
 
-qqnorm(c.sum)
-qqnorm(nc.sum)
+diff.table <- t(cbind(cnt.table[1,], cnt.table[2,] - cnt.table[1,]))
 
-t.test(c.sum, nc.sum)
+chisq.test(cnt.table)
+chisq.test(diff.table)
