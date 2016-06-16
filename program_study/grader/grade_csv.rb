c21b67772ba93e32a2d0e33b43d5fb4c284426eb
Dan Gopstein <dan@gopstein.com>
2016-06-15 23:50:44 -0400

    output csv from csv grader

--- program_study/grader/grade_csv.rb
+++ program_study/grader/grade_csv.rb
@@ -22,17 +22,31 @@ pilot_results = results.select{|r| pilot_ids.include?(r["Subject"].to_i)}.map{|r
 
 compile_graders!
 
-scores = ('a'..'h').each_with_object(Hash.new{[]}) do |q, hash|
-  score = results[q.upcase].compact.map { |s| run_grader(q, s) }
-  hash[q] += score
+#scores = ('a'..'h').each_with_object(Hash.new{[]}) do |q, hash|
+#  score = results[q.upcase].compact.map { |s| run_grader(q, s) }
+#  hash[q] += score
+#end
+
+#scores = ('a'..'h').flat_map do |q|
+#  results[q.upcase].compact.map { |s| [q, results['Subject'], run_grader(q, s)] }
+#end
+
+scores = results.flat_map do |r|
+  ('a'..'h').map do |q|
+    ans = r[q.upcase]
+    ans and [r['Subject'].to_i, q, run_grader(q, ans)]
+  end.compact
 end
 
-score_avgs = scores.map do |type, scores|  
-  num, denom = scores.transpose.map{|x| x.inject(:+)}
+puts "subject,qtype,correct,points"
+puts scores.map{|r| r.flatten.to_csv }
 
-  [type, (num.to_f / denom).round(2)]
-end.to_h
+#score_avgs = scores.map do |type, scores|  
+#  num, denom = scores.transpose.map{|x| x.inject(:+)}
+#
+#  [type, (num.to_f / denom).round(2)]
+#end.to_h
 
-pp score_avgs
+#pp score_avgs
 
 
