0edd2e291b6d9c8be6b6e453aadc055ccdc3bcd0
Dan Gopstein <dan@gopstein.com>
2016-06-16 03:33:13 -0400

    Sample size of combined data (a+c+e+g) vs (b+d+f+h)

--- program_study/grader/sample_size.R
+++ program_study/grader/sample_size.R
@@ -4,29 +4,43 @@ library("samplesize")
 pilot.results <- data.table(read.csv("csv/pilot_results.csv", header = TRUE))
 pilot.results$confusing <- pilot.results$qtype %in% c('a','c','e','g')
 
+scores.summed <- pilot.results[,.(rate=sum(correct)/sum(points)),by=c("subject", "confusing")]
+c.sum <- scores.summed[confusing == TRUE, rate]
+nc.sum <- scores.summed[confusing == FALSE, rate]
+
+# sum.tt <- t.test(c.sum, nc.sum)
+# sum.tss <- n.ttest(power = 0.8, alpha = 0.05, mean.diff = mean(c.sum) - mean(nc.sum), sd1 = sd(c.sum), sd2 = sd(nc.sum))
+# sum.wt <- wilcox.test
+
+
 scores <- function(char) pilot.results[qtype == char, correct/points]
-
-length(scores('a'))
-
-var.test(scores('a'), scores('b'))
 
 cs <- c('a', 'c', 'e', 'g')
 ncs <- c('b', 'd', 'f', 'h')
+
+w.vs.t <- function (as, bs, name = "") {
+  wt <- wilcox.test(as, bs)
+  
+  wss <- n.wilcox.ord(power = 0.8, alpha = 0.05, t = 0.5, p = c(mean(as), 1 - mean(as)), q = c(mean(bs), 1 - mean(bs)))
+  
+  tt <- t.test(as, bs)
+  tss <- n.ttest(power = 0.8, alpha = 0.05, mean.diff = mean(as) - mean(bs), sd1 = sd(as), sd2 = sd(bs))
+  
+  #list("pair" =name, "t.p.value" = tt$p.value, "t.ss" = tss$`Total sample size`, "wilcox.p.value" = wt$p.value, "wilcox.ss" = wss$`total sample size`)
+  res <- c(name, tt$p.value, tss$`Total sample size`, wt$p.value, wss$`total sample size`)
+  names(res) <- c("pair", "t.p.value", "t.ss", "wilcox.p.value", "wilcox.ss")
+  
+  res
+}
 
 pair.test <- function(a, b) {
   as <- scores(a)
   bs <- scores(b)
   
-  wt <- wilcox.test(as, bs)
-
-  wss <- n.wilcox.ord(power = 0.8, alpha = 0.05, t = 0.5, p = c(mean(as), 1 - mean(as)), q = c(mean(bs), 1 - mean(bs)))
-  
-  tt <- t.test(as, bs)
-  tss <- n.ttest(power = 0.8, alpha = 0.05, mean(as) - mean(bs), sd1 = sd(as), sd2 = sd(bs))
-  
-  list("pair" = paste(a, b, sep=""), "t.p.value" = tt$p.value, "t.ss" = tss$`Total sample size`, "wilcox.p.value" = wt$p.value, "wilcox.ss" = wss$`total sample size`)
+  w.vs.t(as, bs, name =  paste(a, b, sep=""))
 }
 
 pair.data <- t(mapply(pair.test, cs, ncs))
-pair.data
 
+print(pair.data)
+print(w.vs.t(c.sum, nc.sum))
