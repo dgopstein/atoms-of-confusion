3401ea62b5a5c38ca4cf08088578fc1395ab3510
Dan Gopstein <dan@gopstein.com>
2016-06-27 21:49:23 -0400

    Split the answer sheet into consent, answers, and survey

---
+++ program_study/grader/answer_splitter.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+input_file=$1
+subject_id=$2
+
+pdftk $input_file cat 1-1     output "${subject_id}_consent.pdf"
+pdftk $input_file cat 2-r2    output "${subject_id}_answers.pdf"
+pdftk $input_file cat end-end output "${subject_id}_survey.pdf"
