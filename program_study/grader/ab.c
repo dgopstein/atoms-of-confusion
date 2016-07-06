f30fa70621549de18603f2b68c6990328eac9399
Dan Gopstein <dan@gopstein.com>
2016-07-06 01:20:19 -0400

    Fix floating point exception

--- program_study/grader/ab.c
+++ program_study/grader/ab.c
@@ -67,7 +67,13 @@ int F1(int V1, char *V2, int V3) {
 
 
 
-  int V4 = (V1 / V3) + V3;
+  int V4;
+  if (V3 != 0) {
+    V4 = (V1 / V3) + V3;
+  } else {
+    V4 = 0;
+  }
+
   char *V5 = V2 - V1;
   V2 = V2 - 1;
   int V6 = (int)V2 / (int)V2;
