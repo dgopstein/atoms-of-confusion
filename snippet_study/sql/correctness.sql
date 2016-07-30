7b64471631509c9abad65e66885380bb47088a46
Dan Gopstein <dan@gopstein.com>
2016-07-30 11:54:35 -0400

    Add correctness sql

---
+++ snippet_study/sql/correctness.sql
@@ -0,0 +1,9 @@
+select t.tag as atom, t.tag||'_'||uc.codeid as question, uc.codeid as qid, c.type,
+SUM(CASE WHEN uc.correct = 'T' THEN 1 ELSE 0 END) as T,
+SUM(CASE WHEN uc.correct = 'F' THEN 1 ELSE 0 END) as F,
+count(*) as total
+from usercode uc
+join code c on uc.codeid = c.id
+join codetags ct on ct.codeid = c.id
+join tag t on ct.tagid = t.id
+group by t.id, qid;
