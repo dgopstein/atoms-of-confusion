24468b012130bb381034e44ba51accd26398e22c
Dan Gopstein <dan@gopstein.com>
2016-08-31 16:11:01 -0400

    Format output for clustered-matched-pair

---
+++ snippet_study/sql/clustered_contingency_normalized.sql
@@ -0,0 +1,17 @@
+-- Select clustered user data to be analyzed by the
+-- clustered-matched-pair package I'm developing
+
+select
+  uc.userid as subject,
+  uc.codeid as question,
+  uc.correct  = 'T' as control,
+  uc2.correct = 'T' as treatment
+from scrubbed_usercode uc
+join code c on uc.codeid = c.id
+join scrubbed_usercode uc2 on uc.userid = uc2.userid and uc2.codeid = c.pair
+join codetags ct on ct.codeid = c.id
+join tag t on ct.tagid = t.id
+where
+uc.userid < 20 AND uc.codeid < 10 AND
+c.type = 'Confusing';
+
