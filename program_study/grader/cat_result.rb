ca3c7bf91c89244abd089a34d33fbf73a01f6c7a
Dan Gopstein <dan@gopstein.com>
2017-01-27 16:04:39 -0500

    print specific question result

---
+++ program_study/grader/cat_result.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+# Print the answer of a specific subject/answer
+
+$cwd = File.expand_path File.dirname(__FILE__)
+
+require "#$cwd/grader_util.rb"
+
+require 'csv' 
+
+# (results_file, s_id, q_id = ['csv/results.csv', 9998, 'h'])
+results_file, s_id, q_id = ARGV
+
+if [results_file, s_id, q_id].any?(&:nil?)
+  puts "Usage: cat_result.rb results_file.csv 9998 h "
+  exit(1)
+end
+
+results = read_results(results_file)
+
+subject = results.find{|row| row['Subject'] == s_id.to_s }
+
+puts subject[q_id.upcase]
